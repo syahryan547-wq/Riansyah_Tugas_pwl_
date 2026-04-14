15da408344b7469ef7601d5b5947edfdba5fcdca
Riansyah <syahryan547@gmail.com>
2026-04-14 20:14:36 +0700

    Create 2026_04_02_065303_create_books_table.php

---
+++ 2026_04_02_065303_create_books_table.php
@@ -0,0 +1,29 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->year('year');
+            $table->string('publisher');
+            $table->string('city');
+            $table->string('cover')->nullable(); // diperbaiki
+
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('bookshelf_id')->constrained('bookshelfs')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
