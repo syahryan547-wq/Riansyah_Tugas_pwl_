aceeee0cc8824f8f32e3be60743d0ff1e8551130
Riansyah <syahryan547@gmail.com>
2026-04-14 20:10:18 +0700

    Create 0001_01_01_000001_create_cache_table.php

---
+++ 0001_01_01_000001_create_cache_table.php
@@ -0,0 +1,27 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->text('value'); // sedikit beda (lebih umum)
+            $table->integer('expiration')->index();
+        });
+
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration')->index();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cache_locks'); // dibalik biar aman
+        Schema::dropIfExists('cache');
+    }
+};
