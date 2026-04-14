3ea4830962811842ae1c0ed44bf2b5685976e7ce
Riansyah <syahryan547@gmail.com>
2026-04-14 20:27:16 +0700

    Update 2026_04_02_065303_create_books_table.php

--- 2026_04_02_065303_create_books_table.php
+++ 2026_04_02_065303_create_books_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->year('year');
             $table->string('publisher');
             $table->string('city');
-            $table->string('cover')->nullable(); // diperbaiki
+            $table->string('cover')->nullable(); 
 
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->foreignId('bookshelf_id')->constrained('bookshelfs')->cascadeOnDelete();
